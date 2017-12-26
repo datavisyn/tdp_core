c697df617b0e4e255e0ac4a1842478068f8158c4
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-12-26 12:08:38 +0100

    lazy load dialog module to avoid jquery in entry chunk

--- src/dialogs.ts
+++ src/dialogs.ts
@@ -3,8 +3,20 @@
  */
 
 import CLUEGraphManager from 'phovea_clue/src/CLUEGraphManager';
-import {generateDialog} from 'phovea_ui/src/dialogs';
+import {IAreYouSureOptions, Dialog, FormDialog} from 'phovea_ui/src/dialogs';
 export {setGlobalErrorTemplate, showErrorModalDialog} from 'phovea_ui/src/errors';
+
+export interface IDialogModule {
+  generateDialog(title: string, primaryBtnText?: string): Dialog;
+
+  areyousure(msg?: string, options?: IAreYouSureOptions | string): Promise<boolean>;
+
+  FormDialog: { new(title: string, primaryBtnText?: string, formId?: string): FormDialog };
+}
+
+export function lazyDialogModule(): Promise<IDialogModule> {
+  return System.import('./dialogs');
+}
 
 /**
  * utility dialog when a session was not found
@@ -12,32 +24,34 @@ export {setGlobalErrorTemplate, showErrorModalDialog} from 'phovea_ui/src/errors
  * @param {string} id session id
  */
 export function showProveanceGraphNotFoundDialog(manager: CLUEGraphManager, id: string) {
-  const dialog = generateDialog('Session Not Found!', 'Create New Temporary Session');
-  // append bg-danger to the dialog parent element
-  dialog.body.parentElement.parentElement.parentElement.classList.add('bg-danger');
-  dialog.body.innerHTML = `
-      <p>
-          The requested session <strong>"${id}"</strong> was not found or is not accessible.
-      </p> 
-      <p>
-          Possible reasons are that you 
-          <ul>
-              <li>requested a <i>temporary session</i> that is already expired</li>
-              <li>tried to access a <i>temporary session</i> of another user</li>
-              <li>tried to access a <i>private persistent session</i> of another user</li>
-          </ul>
-      </p>
-      <p>
-          In the latter two cases, please contact the original owner of the session to create a public persistent session.     
-      </p>`;
-  dialog.onSubmit(() => {
-    dialog.hide();
-    return false;
-  });
-  dialog.onHide(() => {
-    dialog.destroy();
-    manager.newGraph();
-  });
+  lazyDialogModule().then(({generateDialog}) => {
+    const dialog = generateDialog('Session Not Found!', 'Create New Temporary Session');
+    // append bg-danger to the dialog parent element
+    dialog.body.parentElement.parentElement.parentElement.classList.add('bg-danger');
+    dialog.body.innerHTML = `
+        <p>
+            The requested session <strong>"${id}"</strong> was not found or is not accessible.
+        </p> 
+        <p>
+            Possible reasons are that you 
+            <ul>
+                <li>requested a <i>temporary session</i> that is already expired</li>
+                <li>tried to access a <i>temporary session</i> of another user</li>
+                <li>tried to access a <i>private persistent session</i> of another user</li>
+            </ul>
+        </p>
+        <p>
+            In the latter two cases, please contact the original owner of the session to create a public persistent session.     
+        </p>`;
+    dialog.onSubmit(() => {
+      dialog.hide();
+      return false;
+    });
+    dialog.onHide(() => {
+      dialog.destroy();
+      manager.newGraph();
+    });
 
-  dialog.show();
+    dialog.show();
+  });
 }
