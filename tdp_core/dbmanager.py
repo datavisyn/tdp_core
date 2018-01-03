3b541ea43d39020deb31397df39413a2382aa2ff
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-01-03 12:00:39 +0100

    add more utils functions that are used

--- tdp_core/dbmanager.py
+++ tdp_core/dbmanager.py
@@ -1,4 +1,5 @@
 import logging
+
 from phovea_server.config import view as configview
 from phovea_server.plugin import list as list_plugins
 
@@ -9,35 +10,60 @@ _log = logging.getLogger(__name__)
 class DBManager(object):
   def __init__(self):
     self._inited = False
-    self._connectors = list_plugins('tdp-sql-database-definition')
-    self._extensions = list_plugins('tdp-sql-database-extension')
-    self._data = dict()
 
-  def _load_connector(self, item):
+    self.connectors = {}
+    self._plugins = {}
+    self._engines = dict()
+
+    for p in list_plugins('tdp-sql-database-definition'):
+      config = configview(p.configKey)
+      connector = p.load().factory()
+      if not connector.dburl:
+        connector.dburl = config['dburl']
+      if not connector.statement_timeout:
+        connector.statement_timeout = config.get('statement_timeout', default=None)
+      if not connector.statement_timeout_query:
+        connector.statement_timeout_query = config.get('statement_timeout_query', default=None)
+      if not connector.dburl:
+        _log.critical('no db url defined for %s at config key %s - is your configuration up to date?', p.id,
+                      p.configKey)
+        continue
+
+      self._plugins[p.id] = p
+      self.connectors[p.id] = connector
+
+    for p in list_plugins('tdp-sql-database-extension'):
+      base_connector = self.connectors.get(p.base)
+      if not base_connector:
+        _log.critical('invalid database extension no base found: %s base: %s' % (p.id, p.base))
+        continue
+      connector = p.load().factory()
+      if not connector.statement_timeout:
+        connector.statement_timeout = base_connector.statement_timeout
+      if not connector.statement_timeout_query:
+        connector.statement_timeout_query = base_connector.statement_timeout_query
+
+      self._plugins[p.id] = p
+      self.connectors[p.id] = connector
+
+  def _load_engine(self, item):
     if not self._inited:
       self._inited = True
       for p in list_plugins('greenifier'):
         _log.info('run greenifier: %s', p.id)
         p.load().factory()
-    if item in self._data:
-      return self._data[item]
+    if item in self._engines:
+      return self._engines[item]
 
-    p = next(p for p in self._connectors if p.id == item)
-    config = configview(p.configKey)
-    connector = p.load().factory()
-    if not connector.dburl:
-      connector.dburl = config['dburl']
-    if not connector.statement_timeout:
-      connector.statement_timeout = config.get('statement_timeout', default=None)
-    if not connector.statement_timeout_query:
-      connector.statement_timeout_query = config.get('statement_timeout_query', default=None)
+    p = self._plugins[item]
+    if p.type == 'tdp-sql-database-extension':
+      engine = self._load_engine(p.base)
+      self._engines[item] = engine
+      return engine
 
-    if not connector.dburl:
-      _log.critical('no db url connector defined for %s at config key %s - is your configuration up to date?', p.id,
-                    p.configKey)
-      raise NotImplementedError('missing db connector url')
-
+    connector = self.connectors[item]
     _log.info('%s -> %s', p.id, connector.dburl)
+    config = configview(p.configKey)
     engine_options = config.get('engine', default={})
     import sqlalchemy
     engine = sqlalchemy.create_engine(connector.dburl, **engine_options)
@@ -49,45 +75,26 @@ class DBManager(object):
     # https://github.com/kljensen/async-flask-sqlalchemy-example/blob/master/server.py
     engine.pool._use_threadlocal = True
 
-    r = connector, engine
-    self._data[item] = r
-    return r
-
-  def _load_extension(self, item):
-    if item in self._data:
-      return self._data[item]
-
-    p = next(p for p in self._extensions if p.id == item)
-    base = self.get(p.base)
-    if not base:
-      raise NotImplementedError('invalid database extension no base found: %s base: %s' % (p.id, p.base))
-
-    base_connector, engine = base
-    connector = p.load().factory()
-    if not connector.statement_timeout:
-      connector.statement_timeout = base_connector.statement_timeout
-    if not connector.statement_timeout_query:
-      connector.statement_timeout_query = base_connector.statement_timeout_query
-
-    r = connector, engine
-    self._data[item] = r
-    return r
+    self._engines[item] = engine
+    return engine
 
   def __getitem__(self, item):
     if item not in self:
       raise NotImplementedError('missing db connector: ' + item)
-    if item in self._data:
-      return self._data[item]
-    if any(p.id == item for p in self._connectors):
-      return self._load_connector(item)
-    if any(p.id == item for p in self._extensions):
-      return self._load_extension(item)
-    raise NotImplementedError('missing db connector: ' + item)
+    return self.connectors[item], self._load_engine(item)
+
+  def connector(self, item):
+    if item not in self:
+      raise NotImplementedError('missing db connector: ' + item)
+    return self.connectors[item]
+
+  def engine(self, item):
+    if item not in self:
+      raise NotImplementedError('missing db connector: ' + item)
+    return self._load_engine(item)
 
   def __contains__(self, item):
-    return item in self._data or \
-           any(p.id == item for p in self._connectors) or \
-           any(p.id == item for p in self._extensions)
+    return item in self.connectors
 
   def get(self, item, default=None):
     if item not in self:
