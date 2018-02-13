04d974fbb219dbee61d9172b2dbd4242064a6c08
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-02-13 23:10:08 +0100

    implement notifications + use them in save/delete dialogs

---
+++ src/notifications.ts
@@ -0,0 +1,53 @@
+/**
+ * Created by Holger Stitz on 18.08.2016.
+ */
+
+import CLUEGraphManager from 'phovea_clue/src/CLUEGraphManager';
+import {IAreYouSureOptions, Dialog, FormDialog} from 'phovea_ui/src/dialogs';
+export {setGlobalErrorTemplate, showErrorModalDialog} from 'phovea_ui/src/errors';
+
+export function pushNotification(level: 'success'|'info'|'warning'|'danger', msg: string, autoHideInMs = -1) {
+  let parent = <HTMLElement>document.body.querySelector('div.toast-container');
+  if (!parent) {
+    document.body.insertAdjacentHTML('beforeend',`<div class="toast-container"></div>`);
+    parent = <HTMLElement>document.body.lastElementChild!;
+  }
+
+  parent.classList.add('push');
+  parent.insertAdjacentHTML('afterbegin', `<div class="alert alert-${level} alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  ${msg}</div>`);
+
+  const alert = parent.lastElementChild!;
+  // fix link color
+  Array.from(alert.querySelectorAll('a')).forEach((a: HTMLElement) => a.classList.add('alert-link'));
+  // try creating a slide down animation
+  parent.style.top = `-${alert.clientHeight}px`;
+  parent.classList.remove('push');
+  parent.style.top = null;
+
+  if (autoHideInMs > 0) {
+    setTimeout(() => alert.remove(), autoHideInMs);
+  }
+}
+
+export function successfullySaved(type: string, name: string) {
+  pushNotification('success', `${type} "${name}" successfully saved`,3000);
+}
+
+
+export function errorAlert(error: any) {
+  if (error instanceof Response || error.response instanceof Response) {
+    const xhr: Response = error instanceof Response ? error : error.response;
+    return xhr.text().then((body: string) => {
+      if (xhr.status !== 400) {
+        body = `${body}<hr>
+          The requested URL was:<br><a href="${xhr.url}" target="_blank" class="alert-link">${(xhr.url.length > 100) ? xhr.url.substring(0, 100) + '...' : xhr.url}</a>`;
+      }
+      pushNotification('danger', `<strong>Error ${xhr.status} (${xhr.statusText})</strong>: ${body}`);
+    });
+  } else if (error instanceof Error) {
+    pushNotification('danger', `<string>${error.name}</strong>: ${error.message}`);
+  }
+  pushNotification('danger', `<string>Unknown Error</strong>: ${error.toString()}`);
+}
