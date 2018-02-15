dd8185bed7aa4dc7fce648f056e7495e3dd499b5
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-02-15 11:40:20 +0100

    fix #82 option for a readable type

--- src/views/SelectionChooser.ts
+++ src/views/SelectionChooser.ts
@@ -1,27 +1,34 @@
-
-
-import {ISelection} from './interfaces';
-import {FormElementType, IFormElement, IFormElementDesc, IFormSelectElement} from '../form';
-import {IDType, IDTypeLike, resolve} from 'phovea_core/src/idtype';
-import {resolveIds} from './resolve';
 import {randomId} from 'phovea_core/src';
+import {IDType, IDTypeLike, resolve} from 'phovea_core/src/idtype';
+import {FormElementType, IFormElement, IFormElementDesc, IFormSelectElement} from '../form';
+import {ISelection} from './interfaces';
+
+export interface ISelectionChooserOptions {
+  readableIDType: IDTypeLike;
+  label: string;
+  appendOriginalLabel: boolean;
+}
 
 /**
  * helper class for chooser logic
  */
 export default class SelectionChooser {
 
-  private readonly target: IDType|null;
+  private readonly target: IDType | null;
+  private readonly readAble: IDType | null;
   readonly desc: IFormElementDesc;
   private readonly formID: string;
+  private readonly appendOriginalLabel: boolean;
 
-  constructor(private readonly accessor: (id: string) => IFormElement, targetIDType?: IDTypeLike) {
-    this.target = targetIDType ? resolve(targetIDType): null;
+  constructor(private readonly accessor: (id: string) => IFormElement, targetIDType?: IDTypeLike, options: Partial<ISelectionChooserOptions> = {}) {
+    this.target = targetIDType ? resolve(targetIDType) : null;
+    this.readAble = options.readableIDType ? resolve(options.readableIDType) : null;
+    this.appendOriginalLabel = options.appendOriginalLabel == null ? true : options.appendOriginalLabel;
 
     this.formID = `forms.chooser.select.${this.target ? this.target.id : randomId(4)}`;
     this.desc = {
       type: FormElementType.SELECT,
-      label: 'Show',
+      label: options.label || 'Show',
       id: this.formID,
       options: {
         optionsData: [],
@@ -49,32 +56,49 @@ export default class SelectionChooser {
     return {id: parseInt(s.id, 10), name: s.name};
   }
 
-  private updateImpl(selection: ISelection, reuseOld: boolean): Promise<boolean> {
+  private async updateImpl(selection: ISelection, reuseOld: boolean): Promise<boolean> {
     const ids = selection.range.dim(0).asList();
-    return resolveIds(selection.idtype, selection.range, this.target).then((names) => {
-      const options = names.map((name, i) => ({value: ids[i].toString(), name, data: {id: ids[i], name}}));
-      const element = <IFormSelectElement>this.accessor(this.formID);
+    if (this.target == null && this.readAble == null) {
+      const names = await selection.idtype.unmap(selection.range);
+      return this.updateItems(ids.map((id, i) => ({id, label: names[i], name: names[i]})), reuseOld);
+    }
+    const mapped = await selection.idtype.mapToID(selection.range, this.target);
 
-      // backup entry and restore the selectedIndex by value afterwards again,
-      // because the position of the selected element might change
-      const bak = element.value || options[element.getSelectedIndex()];
-      element.updateOptionElements(options);
 
-      let changed = true;
-      // select last item from incoming `selection.range`
-      if (!reuseOld) {
-        element.value = options.filter((d) => d.value === names[names.length - 1])[0];
-        // otherwise try to restore the backup
-      } else if (bak !== null) {
-        element.value = bak;
-        changed = false;
-      }
+    const flatIds = (<number[]>[]).concat(...mapped);
+    const names = await this.target.unmap(flatIds);
+    const labels = this.readAble ? await this.target.mapToFirstName(flatIds, this.readAble) : null;
 
-      // just show if there is more than one
-      element.setVisible(options.length > 1);
+    return this.updateItems(flatIds.map((id, i) => ({
+      id,
+      name: names[i],
+      label: labels ? (this.appendOriginalLabel ? `${labels[i]} (${names[i]})` : labels[i]) : names[i]
+    })), reuseOld);
+  }
 
-      return changed;
-    });
+  private updateItems(items: { label: string, id: number, name: string }[], reuseOld: boolean) {
+    const options = items.map(({label, id, name}) => ({value: id.toString(), name, data: {id, name}}));
+    const element = <IFormSelectElement>this.accessor(this.formID);
+
+    // backup entry and restore the selectedIndex by value afterwards again,
+    // because the position of the selected element might change
+    const bak = element.value || options[element.getSelectedIndex()];
+    element.updateOptionElements(options);
+
+    let changed = true;
+    // select last item from incoming `selection.range`
+    if (!reuseOld) {
+      element.value = options.filter((d) => d.value === items[items.length - 1].id.toString())[0];
+      // otherwise try to restore the backup
+    } else if (bak !== null) {
+      element.value = bak;
+      changed = false;
+    }
+
+    // just show if there is more than one
+    element.setVisible(options.length > 1);
+
+    return changed;
   }
 
 }
