93eadebcfc03eb6f2339559fbb1dbf5a0c5fde72
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-03-08 17:46:16 +0100

    fix #90 support table view sorting

--- src/views/ATableView.ts
+++ src/views/ATableView.ts
@@ -10,6 +10,7 @@ export interface IATableViewOptions {
   stripedRows: boolean;
   bordered: boolean;
   condensed: boolean;
+  sortable: boolean | ((th: HTMLElement, index: number) => boolean | 'number' | 'string');
 }
 
 /**
@@ -21,7 +22,8 @@ export abstract class ATableView<T extends IRow> extends AView {
     selectAble: true,
     stripedRows: false,
     bordered: false,
-    condensed: false
+    condensed: false,
+    sortable: true
   };
 
   /**
@@ -55,7 +57,7 @@ export abstract class ATableView<T extends IRow> extends AView {
   protected initImpl() {
     super.initImpl();
 
-    this.node.innerHTML = `<table class="table table-hover ${this.options.condensed ? ' table-condensed': ''}${this.options.bordered ? ' table-bordered': ''}${this.options.stripedRows ? ' table-striped': ''}">
+    this.node.innerHTML = `<table class="table table-hover ${this.options.condensed ? ' table-condensed' : ''}${this.options.bordered ? ' table-bordered' : ''}${this.options.stripedRows ? ' table-striped' : ''}">
         <thead><tr></tr></thead>
         <tbody></tbody>
     </table>`;
@@ -81,7 +83,7 @@ export abstract class ATableView<T extends IRow> extends AView {
    * load the rows of LineUp
    * @returns {Promise<IRow[]>} the rows at least containing the represented ids
    */
-  protected abstract loadRows(): Promise<T[]>|T[];
+  protected abstract loadRows(): Promise<T[]> | T[];
 
 
   private build() {
@@ -96,14 +98,83 @@ export abstract class ATableView<T extends IRow> extends AView {
       });
   }
 
+  private enableSort(header: HTMLElement, body: HTMLElement) {
+
+    const text = (a: string, b: string) => a.toLowerCase().localeCompare(b.toLowerCase());
+    const number = (a: string, b: string) => {
+      const av = parseFloat(a);
+      const bv = parseFloat(b);
+      if (isNaN(av) && isNaN(bv)) {
+        return text(a, b);
+      }
+      if (isNaN(av)) {
+        return -1;
+      }
+      if (isNaN(bv)) {
+        return +1;
+      }
+      return av - bv;
+    };
+    
+    const sorter = (th: HTMLElement, i: number, sorter = text) => {
+      return () => {
+        const current = th.dataset.sort;
+        const rows = <HTMLElement[]>Array.from(body.children);
+        const next = current === 'no' ? 'asc' : (current === 'asc' ? 'desc' : 'no');
+        th.dataset.sort = next;
+        switch (next) {
+          case 'no':
+            // natural order
+            rows.sort((a, b) => parseInt(a.dataset.i, 10) - parseInt(b.dataset.i, 10));
+            break;
+          case 'desc':
+            rows.sort((a, b) => {
+              const acol = a.children[i];
+              const bcol = b.children[i];
+              return -sorter(acol ? acol.textContent : '', bcol ? bcol.textContent : '');
+            });
+            break;
+          default:
+            rows.sort((a, b) => {
+              const acol = a.children[i];
+              const bcol = b.children[i];
+              return sorter(acol ? acol.textContent : '', bcol ? bcol.textContent : '');
+            });
+        }
+        // readd in ordered sequence
+        body.innerHTML = '';
+        rows.forEach((r) => body.appendChild(r));
+      };
+    };
+
+
+    Array.from(header.children).forEach((d: HTMLElement, i) => {
+      const sort = typeof this.options.sortable === 'function' ? this.options.sortable(d, i) : this.options.sortable;
+      if (!sort) {
+        return;
+      }
+      d.dataset.sort = 'no';
+      if (sort === 'number') {
+        d.dataset.num = '';
+        d.onclick = sorter(d, i, number);
+      } else if (sort) {
+        d.onclick = sorter(d, i);
+      }
+    });
+  }
+
   private renderTable(rows: T[]) {
     const header = <HTMLTableRowElement>this.node.querySelector('thead tr');
     header.innerHTML = '';
     const keys = this.renderHeader(header, rows);
     const body = <HTMLTableSectionElement>this.node.querySelector('tbody');
+    if (this.options.sortable) {
+      this.enableSort(header, body);
+    }
     body.innerHTML = '';
     rows.forEach((row, i) => {
       const tr = body.insertRow();
+      tr.dataset.i = String(i);
       this.renderRow(tr, row, i, keys);
       if (this.options.selectAble && this.itemIDType) {
         tr.onclick = (evt) => {
