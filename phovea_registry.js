3d01bd77f56a82a7aa430424172db6385ca8d8a9
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-07-20 19:44:43 +0200

    fix phovea.ts import

--- phovea_registry.js
+++ phovea_registry.js
@@ -5,11 +5,11 @@
  **************************************************************************** */
 
 import {register} from 'phovea_core/src/plugin';
-
+import reg from './src/phovea';
 /**
  * build a registry by registering all phovea modules
  */
 //other modules
 import 'phovea_clue/phovea_registry.js';
 //self
-register('tdp_core',require('./src/phovea'));
+register('tdp_core', reg);
