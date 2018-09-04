93d500b709be695b5b02947df1fb23bba6cc10b8
Klaus Eckelt <Klaus.Eckelt@jku.at>
2018-09-04 10:18:38 +0200

    Toggle side panel content #122

--- src/lineup/internal/TouringLineUpPanel.ts
+++ src/lineup/internal/TouringLineUpPanel.ts
@@ -1,12 +1,40 @@
 import LineUpPanelActions from './LineUpPanelActions';
 
 export default class TouringLineUpPanel extends LineUpPanelActions {
+
+  private touringElem : HTMLElement;
   
   protected init() {
     super.init();
+    this.node.insertAdjacentHTML('beforeend', `<div class="touring" hidden style="background-color: coral;">
+    <p>Hello World</p>
+    </div>`);
+    this.touringElem = <HTMLElement>this.node.querySelector('.touring');
+
     const buttons = this.node.querySelector('section');
     buttons.appendChild(this.createMarkup('Start Touring', 'fa fa-bar-chart', () => {
       console.log('start touring')
+
+      const columnOverview = <HTMLElement>this.node.querySelector('main')!;
+      const searchbox = <HTMLElement>this.node.querySelector('.lu-adder')!;
+      const itemCounter = <HTMLElement>this.node.querySelector('.lu-stats')!;
+
+      if (columnOverview.hidden === true) {
+        searchbox.hidden = false;
+        itemCounter.hidden = false;
+        columnOverview.hidden = false;
+
+        this.touringElem.hidden = true;
+      } else {
+        searchbox.hidden = true;
+        itemCounter.hidden = true;
+        columnOverview.hidden = true;
+
+        this.touringElem.hidden = false;
+      }
+       
+
+
     }));
   }
 }
