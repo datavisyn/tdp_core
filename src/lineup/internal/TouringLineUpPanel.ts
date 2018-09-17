db9d55f661b8f40aa2af5be5523d250a2ef224df
Klaus Eckelt <Klaus.Eckelt@jku.at>
2018-09-17 12:39:25 +0200

    import touring panel html code with webpack, #122

--- src/lineup/internal/TouringLineUpPanel.ts
+++ src/lineup/internal/TouringLineUpPanel.ts
@@ -1,4 +1,5 @@
 import LineUpPanelActions from './LineUpPanelActions';
+import panelHTML from 'html-loader!./TouringPanel.html'; // webpack imports html to variable
 
 export default class TouringLineUpPanel extends LineUpPanelActions {
 
@@ -7,104 +8,8 @@ export default class TouringLineUpPanel extends LineUpPanelActions {
   
   protected init() {
     super.init();
-    this.node.insertAdjacentHTML('beforeend', `  <div class="touring" hidden>
-    <ul class="nav nav-tabs nav-justified">
-      <li class="active"><a href="#itemTouring" data-toggle="tab">Items</a></li>
-      <li><a href="#attributeTouring" data-toggle="tab">Attributes</a></li>
-    </ul>
-
-    <div class="tab-content">
-      <div class="tab-pane active" id="itemTouring">
-        <div class="form-inline">
-          <label for="compareSrc1">Compare:</label>
-          <select class="form-control" id="compareSrc1">
-              <option>Selected Rows</option>
-              <option>A Int < 0.33 </option>
-              <option>0.33 <= A Int <= 0.66</option>
-              <option>A Int > 0.66</option>
-          </select>
-        </div>
-        
-        <div class="form-inline">
-          <label>With:</label>
-          <label>
-            <input type="radio" name="cmpGroup" value="category" checked> Categories
-          </label>
-          <label>
-            <input type="radio" name="cmpGroup" value="group"> Stratification Groups
-          </label>
-        </div>
-        
-        <div class="form-inline">
-          <label for="compareSrc2">Of:</label>
-          <select class="form-control" id="compareSrc2">
-            <option>All displayed attributes</option>
-            <option>All categorical attributes</option>
-            <option>Selection</option>
-            <option>Stratification</option>
-            <option>A Cat1</option>
-            <option>A Cat2</option>
-          </select>
-        </div>
-        <hr>
-        <div>
-          <h4>Jaccard Similarity</h1>
-          <div class="table-responsive">
-            <table class="table table-bordered table-condensed table-hover">
-              <tr>
-                <th></th>
-                <th></th>
-                <th>Selected</th>
-              </tr>
-              <tr>
-                <td>A Cat1</td>
-                <td>A Cat1</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>A Cat2</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>A Cat3</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>A Cat2</td>
-                <td>A CatB 1</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>A CatB 2</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>A CatB 3</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>A CatB 4</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>A CatB 5</td>
-                <td></td>
-              </tr>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="tab-pane" id="attributeTouring">
-        ,,,
-      </div>
-    </div>
-  </div>`);
+    this.node
+    this.node.insertAdjacentHTML('beforeend', panelHTML);
     this.touringElem = <HTMLElement>this.node.querySelector('.touring');
     
     this.columnOverview = <HTMLElement>this.node.querySelector('main')!;
@@ -113,20 +18,15 @@ export default class TouringLineUpPanel extends LineUpPanelActions {
 
     const buttons = this.node.querySelector('section');
     buttons.appendChild(this.createMarkup('Start Touring', 'fa fa-bar-chart', () => {
-      console.log('start touring')
-      
       this.toggleTouring();
-
-      // TODO adder as dropdown?
-      // TODO restore panel on touring click
     }));
   }
-
+  
   
   private toggleTouring(hide?: boolean) {
     if(!this.touringElem)
-      return; // the elements are undefined
-
+    return; // the elements are undefined
+    
     if (hide === undefined) {
       // if not hidden -> hide
       hide =!this.touringElem.hidden;
@@ -138,6 +38,7 @@ export default class TouringLineUpPanel extends LineUpPanelActions {
     
     this.touringElem.hidden = hide;
     if (!hide) {
+      console.log('start touring')
       //if touring is displayed, ensure the panel is visible
       this.collapse = false;
     }
