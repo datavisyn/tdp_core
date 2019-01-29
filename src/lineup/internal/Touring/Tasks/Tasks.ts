8a7a9f4bdb6b4a5ce940cfc8fa4976040acb96ba
Klaus Eckelt <Klaus.Eckelt@jku.at>
2019-01-29 14:43:41 +0100

    Highlight after short delay Caleydo/tdp_bi_bioinfodb#963

--- src/lineup/internal/Touring/Tasks/Tasks.ts
+++ src/lineup/internal/Touring/Tasks/Tasks.ts
@@ -71,6 +71,8 @@ export abstract class ATouringTask implements ITouringTask {
 
   ranking: RankingAdapter;
 
+  private hoverTimerId: number;
+
   public abort() {
     WorkerManager.terminateAll();
   }
@@ -432,31 +434,34 @@ export abstract class ATouringTask implements ITouringTask {
       const cellData = d3.select(tableCell).datum() as IScoreCell;
       if (cellData && cellData.highlightData) {
         if (state) {
-          // highlight col headers
-          let id;
-          for (const attr of cellData.highlightData.filter((data) => data.category === undefined)) {
-            const header = d3.select(`.lineup-engine header .lu-header[title^="${attr.label}"]`).classed('touring-highlight', true); // |= starts with whole word (does not work for selection checkboxes)
-            id = header.attr('data-col-id');
-          }
+          this.hoverTimerId = window.setTimeout(() => { //highlight after 800ms if mouse is still on cell
+            // highlight col headers
+            let id;
+            for (const attr of cellData.highlightData.filter((data) => data.category === undefined)) {
+              const header = d3.select(`.lineup-engine header .lu-header[title^="${attr.label}"]`).classed('touring-highlight', true); // |= starts with whole word (does not work for selection checkboxes)
+              id = header.attr('data-col-id');
+            }
 
-          if (id) {
-            // highlight cat rows
-            for (const attr of cellData.highlightData.filter((data) => data.category !== undefined)) {
-              const indices = this.ranking.getAttributeDataDisplayed(attr.column).reduce((indices,cat,index) => cat === attr.category ? [...indices, index] : indices, []);
-              for (const index of indices) {
-                const elem = d3.select(`.lineup-engine main .lu-row[data-index="${index}"] [data-id="${id}"]`);
-                if (!elem.empty()) {
-                  const setDarker = elem.classed('touring-highlight');
-                  elem.classed('touring-highlight', true)
-                      .classed('touring-highlight-dark', setDarker);
+            if (id) {
+              // highlight cat rows
+              for (const attr of cellData.highlightData.filter((data) => data.category !== undefined)) {
+                const indices = this.ranking.getAttributeDataDisplayed(attr.column).reduce((indices,cat,index) => cat === attr.category ? [...indices, index] : indices, []);
+                for (const index of indices) {
+                  const elem = d3.select(`.lineup-engine main .lu-row[data-index="${index}"] [data-id="${id}"]`);
+                  if (!elem.empty()) {
+                    const setDarker = elem.classed('touring-highlight');
+                    elem.classed('touring-highlight', true)
+                        .classed('touring-highlight-dark', setDarker);
 
-                  const catId = d3.select(`.lineup-engine header .lu-header[title^="${attr.label}"]`).attr('data-col-id');
-                  d3.select(`.lineup-engine main .lu-row[data-index="${index}"] [data-id="${catId}"]`).classed('touring-highlight-border', true);
+                    const catId = d3.select(`.lineup-engine header .lu-header[title^="${attr.label}"]`).attr('data-col-id');
+                    d3.select(`.lineup-engine main .lu-row[data-index="${index}"] [data-id="${catId}"]`).classed('touring-highlight-border', true);
+                  }
                 }
               }
             }
-          }
+          }, 200);
         } else {
+          window.clearTimeout(this.hoverTimerId);
           d3.selectAll('.touring-highlight,.touring-highlight-dark,.touring-highlight-border').classed('touring-highlight touring-highlight-dark touring-highlight-border', false);
         }
       }
