1e91998eea28bebad81a672e482891525502e18c
Samuel Gratzl <sam@sgratzl.com>
2019-02-19 12:15:15 +0100

    support for manually adding score columns

--- src/base/AEmbeddedRanking.ts
+++ src/base/AEmbeddedRanking.ts
@@ -1,10 +1,12 @@
 import {IColumnDesc, LocalDataProvider} from 'lineupjs';
 import {ProvenanceGraph} from 'phovea_core/src/provenance';
 import {none, parse} from 'phovea_core/src/range';
-import {ARankingView, IARankingViewOptions, IRow} from '../lineup';
+import {ARankingView, IARankingViewOptions, IRow, IScore} from '../lineup';
 import {IInitialRankingOptions} from '../lineup/desc';
 import {IViewProvider} from '../lineup/internal/cmds';
 import {resolve} from 'phovea_core/src/idtype';
+import {EXTENSION_POINT_TDP_SCORE_IMPL} from '../extensions';
+import {get as getPlugin} from 'phovea_core/src/plugin';
 
 
 interface IEmbeddedRanking extends ARankingView {
@@ -147,6 +149,20 @@ export abstract class AEmbeddedRanking<T extends IRow> implements IViewProvider 
 
   protected runWithoutTracking<T>(f: (lineup: LocalDataProvider) => T): Promise<T> {
     return this.ranking.runWithoutTracking(() => f(this.data));
+  }
+
+  protected addTrackedScoreColumn(scoreId: string, scoreParams: any);
+  protected addTrackedScoreColumn(score: IScore<any> | string, scoreParams: any) {
+    if (typeof score !== 'string') {
+      return this.ranking.addTrackedScoreColumn(score);
+    }
+
+    const pluginDesc = getPlugin(EXTENSION_POINT_TDP_SCORE_IMPL, score);
+    return pluginDesc.load().then((plugin) => {
+      const instance: IScore<any> | IScore<any>[] = plugin.factory(scoreParams, pluginDesc);
+      const scores = Array.isArray(instance) ? instance : [instance];
+      return Promise.all(scores.map((s) => this.ranking.addTrackedScoreColumn(s)));
+    });
   }
 
   update() {
