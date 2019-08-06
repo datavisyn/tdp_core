02cbd364e90d0d78f93d7e77deba95cb97782605
Holger Stitz <holger.stitz@jku.at>
2019-08-06 15:58:10 +0200

    Add documentation

--- src/form/internal/index.ts
+++ src/form/internal/index.ts
@@ -1,11 +1,19 @@
 /**
  * Created by Samuel Gratzl on 08.03.2017.
  */
-import {IFormParent, IFormElementDesc} from '../interfaces';
+import {IFormParent, IFormElementDesc,IFormElement} from '../interfaces';
 import {list, IPluginDesc} from 'phovea_core/src/plugin';
 import {FORM_EXTENSION_POINT} from '..';
 
-export function create(parent: IFormParent, $parent: d3.Selection<any>, desc: IFormElementDesc) {
+/**
+ * Factory method to create form elements for the phovea extension type `tdpFormElement`.
+ * An element is found when `desc.type` is matching the extension id.
+ *
+ * @param parent parent form element
+ * @param $parent parent D3 selection element
+ * @param desc form element description
+ */
+export function create(parent: IFormParent, $parent: d3.Selection<any>, desc: IFormElementDesc): Promise<IFormElement> {
   const plugins = list((pluginDesc: IPluginDesc) => {
     return pluginDesc.type === FORM_EXTENSION_POINT && pluginDesc.id === desc.type;
   });
