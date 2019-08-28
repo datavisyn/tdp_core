34d09b68d90b4eb1c7b1381b9777dafe9d8ad6bd
PatrickAdelberger <patrick.adelberger@jku.at>
2019-08-28 12:18:27 +0200

    fix flake errors; add whitespace after comma in abort function

--- tdp_core/db.py
+++ tdp_core/db.py
@@ -347,7 +347,7 @@ def get_filtered_data(database, view_name, args):
   try:
     replacements, processed_args, extra_args, where_clause = filter_logic(view, args)
   except RuntimeError as error:
-    abort(400,error.message)
+    abort(400, error.message)
 
   return get_data(database, view_name, replacements, processed_args, extra_args, where_clause)
 
@@ -359,7 +359,7 @@ def get_filtered_query(database, view_name, args):
   try:
     replacements, processed_args, extra_args, where_clause = filter_logic(view, args)
   except RuntimeError as error:
-    abort(400,error.message)
+    abort(400, error.message)
 
   return get_query(database, view_name, replacements, processed_args, extra_args)
 
@@ -370,7 +370,7 @@ def _get_count(database, view_name, args):
   try:
     replacements, processed_args, extra_args, where_clause = filter_logic(view, args)
   except RuntimeError as error:
-    abort(400,error.message)
+    abort(400, error.message)
 
   kwargs, replace = prepare_arguments(view, config, replacements, processed_args, extra_args)
 
