95bd97c39df68ef82417913e4166f01baa51da6a
Christian Lehner <christian.lehner@datavisyn.io>
2019-09-16 13:59:33 +0200

    added rel=noopener to target=_blank links

--- src/views/ProxyView.ts
+++ src/views/ProxyView.ts
@@ -181,7 +181,7 @@ export default class ProxyView extends AD3View {
       return;
     }
 
-    this.openExternally.innerHTML = `The web page below is directly loaded from <a href="${url}" target="_blank"><i class="fa fa-external-link"></i>${url.startsWith('http') ? url: `${location.protocol}${url}`}</a>`;
+    this.openExternally.innerHTML = `The web page below is directly loaded from <a href="${url}" target="_blank" rel="noopener"><i class="fa fa-external-link"></i>${url.startsWith('http') ? url: `${location.protocol}${url}`}</a>`;
 
     //console.log('start loading', this.$node.select('iframe').node().getBoundingClientRect());
     this.$node.append('iframe')
@@ -214,8 +214,8 @@ export default class ProxyView extends AD3View {
     this.$node.html(`
         <p><div class="alert alert-info center-block" role="alert" style="max-width: 40em"><strong>Security Information: </strong>This website uses HTTPS to secure your communication with our server.
             However, the requested external website doesn't support HTTPS and thus cannot be directly embedded in this application.
-            Please use the following <a href="${url}" target="_blank" class="alert-link">link</a> to open the website in a separate window:
-            <br><br><a href="${url}" target="_blank" class="alert-link">${url}</a>
+            Please use the following <a href="${url}" target="_blank" rel="noopener" class="alert-link">link</a> to open the website in a separate window:
+            <br><br><a href="${url}" target="_blank" rel="noopener" class="alert-link">${url}</a>
         </div></p><p></p>`);
     this.openExternally.innerHTML = ``;
     this.fire(ProxyView.EVENT_LOADING_FINISHED);
