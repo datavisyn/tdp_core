dc86a385eea29d990791e780e225fc0cc73f741e
Christian Lehner <christian.lehner@datavisyn.io>
2019-09-17 15:03:23 +0200

    added re-export of Select3 (and other named exports) for backwards compatiblity

---
+++ src/form/internal/Select3.ts
@@ -0,0 +1,5 @@
+// deprecated: please import Select3 directly from the correct folder (elements/Select3)
+import Select3 from '../elements/Select3';
+export default Select3;
+
+export {IdTextPair, ISelect3Item, ISelect3Group, ISelect3Options, highlightMatch, splitEscaped, equalArrays} from '../elements/Select3';
