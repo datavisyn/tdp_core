3681171814e9acd023cd3328359300162820ceee
oltionchampari <oltion.champari@datavisyn.io>
2019-11-22 09:58:08 +0100

    flattenned translation key path

--- src/storage/editDialog.ts
+++ src/storage/editDialog.ts
@@ -6,21 +6,21 @@ import i18next from 'phovea_core/src/i18n';
 
 export default function editDialog(namedSet: IStoredNamedSet, result: (name: string, description: string, sec: Partial<ISecureItem>) => void) {
   const isCreate = namedSet === null;
-  const title = isCreate ? i18next.t('tdp:core.lineup.editDialog.save') : i18next.t('tdp:core.lineup.editDialog.edit');
-  const dialog = new FormDialog(title + i18next.t('tdp:core.lineup.editDialog.listOfEntities'), title, 'namedset_form');
+  const title = isCreate ? i18next.t('tdp:core.editDialog.save') : i18next.t('tdp:core.editDialog.edit');
+  const dialog = new FormDialog(title + i18next.t('tdp:core.editDialog.listOfEntities'), title, 'namedset_form');
 
   const permissions = permissionForm(namedSet);
 
   dialog.form.innerHTML = `
-    <p> ${i18next.t('tdp:core.lineup.editDialog.dialogText1')}</p>
-    <p style="margin-bottom: 15px">${i18next.t('tdp:core.lineup.editDialog.dialogText2')}</p>
+    <p> ${i18next.t('tdp:core.editDialog.dialogText1')}</p>
+    <p style="margin-bottom: 15px">${i18next.t('tdp:core.editDialog.dialogText2')}</p>
     <div class="form-group">
-      <label for="namedset_name">${i18next.t('tdp:core.lineup.editDialog.name')}</label>
-      <input type="text" class="form-control" name="name" id="namedset_name" placeholder="${i18next.t('tdp:core.lineup.editDialog.name')}" required="required" ${namedSet ? `value="${namedSet.name}"` : ''}>
+      <label for="namedset_name">${i18next.t('tdp:core.editDialog.name')}</label>
+      <input type="text" class="form-control" name="name" id="namedset_name" placeholder="${i18next.t('tdp:core.editDialog.name')}" required="required" ${namedSet ? `value="${namedSet.name}"` : ''}>
     </div>
     <div class="form-group">
-      <label for="namedset_description">${i18next.t('tdp:core.lineup.editDialog.description')}</label>
-      <textarea class="form-control" name="description" id="namedset_description" rows="5" placeholder="${i18next.t('tdp:core.lineup.editDialog.description')}">${namedSet ? namedSet.description : ''}</textarea>
+      <label for="namedset_description">${i18next.t('tdp:core.editDialog.description')}</label>
+      <textarea class="form-control" name="description" id="namedset_description" rows="5" placeholder="${i18next.t('tdp:core.editDialog.description')}">${namedSet ? namedSet.description : ''}</textarea>
     </div>
   `;
   dialog.form.appendChild(permissions.node);
