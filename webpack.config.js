c6fcd22eb5998c0400114206b5dc2a946d823e31
Holger Stitz <holger.stitz@datavisyn.io>
2019-12-03 13:33:34 +0100

    Revert jsx and jsxFactory

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,9 @@ const tsLoaderDev = [
     options: {
       happyPackMode: true, // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack,
       compilerOptions: {
-        target: 'es6'
+        target: 'es6',
+        jsx: 'react',
+        jsxFactory: 'h',
       }
     }
   }
