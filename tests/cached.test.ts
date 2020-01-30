04ee4a616e5d89eeba9d21de4d5e1d603eb7e61c
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-01-30 12:57:43 +0100

    remove check for number

--- tests/cached.test.ts
+++ tests/cached.test.ts
@@ -5,7 +5,6 @@ describe('cached', () => {
   describe('cached', () => {
     it('value', () => {
       expect(cached('test', () => 5)).toBe(5);
-      expect(cached('test', () => 10)).toBe(5);
       expect(cached('test', () => 10)).toBe('using cached value');
     });
   });
