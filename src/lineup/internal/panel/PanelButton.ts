9bc87b1d2e9e5f84e39e45823aa27fbc02cfdf9b
Holger Stitz <holger.stitz@datavisyn.io>
2020-02-07 14:20:51 +0100

    Add documentation to PanelButton

--- src/lineup/internal/panel/PanelButton.ts
+++ src/lineup/internal/panel/PanelButton.ts
@@ -1,11 +1,26 @@
-
+/**
+ * Interface for the LineUp panel button
+ */
 export interface IPanelButton {
+  /**
+   * DOM node of the LineUp panel button
+   */
   readonly node: HTMLElement;
 }
 
+/**
+ * Plain HTML button with a custom title, CSS class and an onClick function
+ */
 export default class PanelButton implements IPanelButton {
   readonly node: HTMLElement;
 
+  /**
+   * Constructor of the PanelButton
+   * @param parent The parent HTML DOM element
+   * @param title String that is used for the title attribute
+   * @param linkClass CSS classes to apply
+   * @param onClick Function that should be executed on button click
+   */
   constructor(parent: HTMLElement, title: string, linkClass: string, onClick: () => void) {
     this.node = parent.ownerDocument.createElement('button');
     this.node.className = linkClass;
@@ -17,6 +32,7 @@ export default class PanelButton implements IPanelButton {
     });
   }
 }
+
 export class PanelNavButton implements IPanelButton {
   readonly node: HTMLElement;
 
