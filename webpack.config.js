8a4eb6ad78dbf0c0f9fee79e010a14da9e04fcb7
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-03-27 08:57:50 +0100

    add gif where necessary

--- webpack.config.js
+++ webpack.config.js
@@ -76,7 +76,7 @@ const webpackloaders = [
   },
   {test: /\.json$/, use: 'json-loader'},
   {
-    test: /\.(png|jpg)$/,
+    test: /\.(png|jpg|gif)$/,
     loader: 'url-loader',
     options: {
       limit: 10000 // inline <= 10kb
