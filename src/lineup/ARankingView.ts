c3750627b49c5ec53e4d9a58e334dc1fdba2ad45
Holger Stitz <holger.stitz@datavisyn.io>
2020-05-13 11:20:34 +0200

    Refactor the RULE_CHANGE to TOGGLE_OVERVIEW event
    Closes #365
    
    Previously, the group height is set as hard-coded values and ignores the LineUp/Taggle default config values.
    
    This refactoring uses the taggle options (LineUp default config + ARankingView custom options).
    
    The event `LineUpPanelActions.EVENT_RULE_CHANGED` was removed and replaced with the `LineUpPanelActions.EVENT_TOGGLE_OVERVIEW` which simply returns a boolean. Afterwards the space-filling rule is only instanciated and used in ARankingView.

--- src/lineup/ARankingView.ts
+++ src/lineup/ARankingView.ts
@@ -1,4 +1,4 @@
-import {EngineRenderer, defaultOptions, updateLodRules, IRule, IGroupData, IGroupItem, isGroup, Column, IColumnDesc, LocalDataProvider, deriveColors, TaggleRenderer, ITaggleOptions, ILocalDataProviderOptions, IDataProviderOptions, IDataRow} from 'lineupjs';
+import {EngineRenderer, defaultOptions, updateLodRules, IRule, IGroupData, IGroupItem, isGroup, Column, IColumnDesc, LocalDataProvider, deriveColors, TaggleRenderer, ITaggleOptions, ILocalDataProviderOptions, IDataProviderOptions, IDataRow, spaceFillingRule} from 'lineupjs';
 import {AView} from '../views/AView';
 import {EViewMode, IViewContext, ISelection} from '../views';
 
@@ -15,7 +15,7 @@ import {debounce, mixin, resolveImmediately} from 'phovea_core/src';
 import LineUpColors from './internal/LineUpColors';
 import {IRow, IServerColumn, IServerColumnDesc} from '../rest';
 import {IContext, ISelectionAdapter, ISelectionColumn} from './selection';
-import LineUpPanelActions, {rule} from './internal/LineUpPanelActions';
+import LineUpPanelActions from './internal/LineUpPanelActions';
 import {addLazyColumn, ILazyLoadedColumn} from './internal/column';
 import {successfullySaved} from '../notifications';
 import {ISecureItem} from 'phovea_core/src/security';
@@ -278,10 +278,10 @@ export abstract class ARankingView extends AView {
     } else if (typeof this.options.itemRowHeight === 'function') {
       const f = this.options.itemRowHeight;
       taggleOptions.dynamicHeight = () => ({
-        defaultHeight: 18,
+        defaultHeight: taggleOptions.rowHeight,
         padding: () => 0,
         height: (item: IGroupItem | IGroupData) => {
-          return isGroup(item) ? 70 : f((<Partial<IDataRow & IGroupItem>>item).v, (<Partial<IDataRow & IGroupItem>>item).i);
+          return isGroup(item) ? taggleOptions.groupHeight : f((<Partial<IDataRow & IGroupItem>>item).v, (<Partial<IDataRow & IGroupItem>>item).i);
         }
       });
     }
@@ -315,13 +315,15 @@ export abstract class ARankingView extends AView {
       this.taggle.zoomIn();
     });
     if (this.options.enableOverviewMode) {
-      this.panel.on(LineUpPanelActions.EVENT_RULE_CHANGED, (_event: any, rule: IRule) => {
-        updateLodRules(this.taggle.style, rule != null, config);
-        (<TaggleRenderer>this.taggle).switchRule(rule);
+      const rule = spaceFillingRule(taggleOptions);
+
+      this.panel.on(LineUpPanelActions.EVENT_TOGGLE_OVERVIEW, (_event: any, isOverviewActive: boolean) => {
+        updateLodRules(this.taggle.style, isOverviewActive, taggleOptions);
+        (<TaggleRenderer>this.taggle).switchRule(isOverviewActive ? rule : null);
       });
+
       if (this.options.enableOverviewMode === 'active') {
-        updateLodRules(this.taggle.style, true, config);
-        (<TaggleRenderer>this.taggle).switchRule(rule);
+        this.panel.fire(LineUpPanelActions.EVENT_TOGGLE_OVERVIEW, true);
       }
     }
 
