ea0908a52ae98afb57f40e9f788e948cb446adc7
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 13:14:14 +0200

    remove deprecated folder

--- src/form/internal/Select3.ts
+++
@@ -1,5 +0,0 @@
-// deprecated: please import Select3 directly from the correct folder (elements/Select3)
-import Select3 from '../elements/Select3';
-export default Select3;
-
-export {IdTextPair, ISelect3Item, ISelect3Group, ISelect3Options, highlightMatch, splitEscaped, equalArrays} from '../elements/Select3';
