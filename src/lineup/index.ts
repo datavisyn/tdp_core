aa2b4b60203026ca152a3648cf29ca042c541d6e
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 14:15:12 +0200

    update index files

--- src/lineup/index.ts
+++ src/lineup/index.ts
@@ -1,11 +1,10 @@
 /**
  * Created by sam on 13.02.2017.
  */
+export * from './internal/internal';
+export * from './selection';
 
-export {default as ARankingView, IARankingViewOptions, IRankingWrapper} from './ARankingView';
-export {ISelectionAdapter, multi, single} from './selection';
-export {IScore, IScoreRow, IScoreParam} from '../extensions';
-export {numberCol, stringCol, categoricalCol, booleanCol, numberColFromArray, linkCol, IAdditionalColumnDesc} from './desc';
-export {IRow} from '../rest';
-export {IViewProvider} from './internal/scorecmds';
-export {toFilterString, toFilter, previewFilterHint} from './internal/utils';
+export * from './ARankingView';
+export {default as ARankingView} from './ARankingView';
+export * from './AOverviewDetailRankingView';
+export * from './desc';
---
+++ src/lineup/selection/internal/internal.ts
@@ -0,0 +1,6 @@
+export * from './ABaseSelectionAdapter';
+export {default as ABaseSelectionAdapter} from './ABaseSelectionAdapter';
+export * from './MultiSelectionAdapter';
+export {default as MultiSelectionAdapter} from './MultiSelectionAdapter';
+export * from './SingleSelectionAdapter';
+export {default as SingleSelectionAdapter} from './SingleSelectionAdapter';
