827efd83ba0d58ed52921a4c5c950a2c63d53e83
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 14:54:52 +0200

    update main index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 
 
-export {ATDPApplication as default} from './ATDPApplication';
+export * from './base';
+export * from './form';
+export * from './lineup';
+export * from './public/search';
+export * from './storage';
+export * from './tour';
+export * from './utils';
+export * from './views';
+
+export * from './ATDPApplication';
+export {default as ATDPApplication} from './ATDPApplication';
+export * from './cached';
+export {default as cached} from './cached';
+export * from './constants';
+export * from './dialogs';
+export * from './extensions';
+export * from './notifications';
+export {default} from './phovea';
+export * from './rest';
+export * from './SessionList';
