e5346f88e35879b38e261548ca1f84cb13042c42
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-27 14:51:22 +0200

    export SessionList

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -1,5 +1,6 @@
 export * from './XlsxUtils';
 export * from './TDPApplicationUtils';
+export * from './SessionList';
 export * from './ProvenanceGraphMenuUtils';
 export * from './EditProvenanceGraphMenu';
 export * from './PhoveaDataAdapter';
