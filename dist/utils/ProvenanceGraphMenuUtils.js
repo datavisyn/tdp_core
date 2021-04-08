2c33ee013f6ea1b097010f556d07f362f8c4e82e
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2021-04-08 08:33:58 +0200

    use checkbox instead of radio

--- dist/utils/ProvenanceGraphMenuUtils.js
+++ dist/utils/ProvenanceGraphMenuUtils.js
@@ -39,9 +39,9 @@ export class ProvenanceGraphMenuUtils {
                 <label for="${prefix}_desc">${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.description')}</label>
                 <textarea class="form-control" id="${prefix}_desc" rows="3">${d.description || ''}</textarea>
               </div>
-                <div class="checkbox custom-control custom-radio custom-control-inline">
-                  <input type="radio" id="customRadio1" name="${prefix}_agree" required="required" class="custom-control-input">
-                  <label class="radio-inline custom-control-label" for="customRadio1">${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.confirmMessage')} <strong>'${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.openExisting')}'</strong> ${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.dialog')}.</label>
+                <div class="checkbox custom-control custom-checkbox custom-control-inline">
+                  <input type="checkbox"" id="customCheckbox1" name="${prefix}_agree" required="required" class="custom-control-input">
+                  <label class="radio-inline custom-control-label" for="customCheckbox1">${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.confirmMessage')} <strong>'${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.openExisting')}'</strong> ${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.dialog')}.</label>
               </div>
           `;
             dialog.form.lastElementChild.insertAdjacentElement('beforebegin', permissions.node);
