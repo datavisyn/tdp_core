82323f5b95114fc6ae167d555757e37a57c3c53b
Holger Stitz <holger.stitz@datavisyn.io>
2021-08-02 22:06:44 +0200

    Fix external link in parameter form of ProxyView

--- dist/views/ProxyView.js
+++ dist/views/ProxyView.js
@@ -40,7 +40,7 @@ export class ProxyView extends AD3View {
         const initResult = await super.init(params, onParameterChange);
         // inject stats
         const base = params.querySelector('form') || params;
-        base.insertAdjacentHTML('beforeend', `<div class="mb-3"></div>`);
+        base.insertAdjacentHTML('beforeend', `<div class="col"></div>`);
         base.lastElementChild.appendChild(this.openExternally);
         return initResult;
     }
