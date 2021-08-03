9aca6f8082f83ef8990724e115da1ae5a82ef32f
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2021-08-03 07:07:30 +0200

    add spacing to checkbox

--- src/form/elements/FormCheckBox.ts
+++ src/form/elements/FormCheckBox.ts
@@ -41,7 +41,7 @@ export class FormCheckBox extends AFormElement<ICheckBoxElementDesc> {
   build($formNode: d3.Selection<any>) {
     this.addChangeListener();
 
-    this.$node = $formNode.append('div').classed('form-check checkbox col-sm-auto', true);
+    this.$node = $formNode.append('div').classed('form-check checkbox col-sm-auto mt-3', true);
     this.setVisible(this.elementDesc.visible);
 
     const $label = this.$node.select('label');
