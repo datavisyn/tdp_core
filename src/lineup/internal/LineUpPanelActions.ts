dc5f747b247eb8679112e95b0c92afcd8b69b3a3
Holger Stitz <holger.stitz@datavisyn.io>
2021-09-29 15:24:31 +0200

    Remove `gap` class from open vis button

--- src/lineup/internal/LineUpPanelActions.ts
+++ src/lineup/internal/LineUpPanelActions.ts
@@ -189,7 +189,7 @@ export class LineUpPanelActions extends EventHandler {
     if (this.options.enableCustomVis) {
       const customVis = new PanelButton(buttons, {
         title: I18nextManager.getInstance().i18n.t('tdp:core.lineup.LineupPanelActions.openVis'),
-        faIcon: 'fas fa-chart-bar gap',
+        faIcon: 'fas fa-chart-bar',
         onClick: () => this.fire(LineUpPanelActions.EVENT_OPEN_VIS)
       });
       this.header.addButton(customVis, EPanelHeaderToolbar.END);
