d3078e5681515228799b9fe577d6138b4e0802eb
Markus Vogl <markus.vogl@datavisyn.io>
2021-11-09 10:41:36 +0100

    Add checckbox filter Fix errors/warnings, add typing info

--- tdp_core/cdc/filter.py
+++ tdp_core/cdc/filter.py
@@ -1,14 +1,15 @@
-from re import match
-from operator import and_, or_, eq
 from functools import reduce
-from typing import Dict, Callable, List, Any
+from operator import and_, or_, eq
+from re import match
+from typing import Dict, List, Any
 
 from marshmallow import Schema, post_load, INCLUDE, fields
 
 
-# This class contains all filters to avoid cyclic dependencies
+# This method contains all filter classes to avoid cyclic dependencies
 class Filter(Schema):
   """ Filter baseclass to delegate to children """
+
   class Meta:
     unknown = INCLUDE
 
@@ -17,12 +18,11 @@ class Filter(Schema):
   componentId = fields.String(required=False)  # "type" of the filter: group, checkbox, text...
 
   @post_load()
-  def _postload(self, data, **_) -> Callable:
+  def _postload(self, data: Dict, **_) -> Dict:
     """ Call a postload function in every sub-class """
     return self.postload(data, **data)
 
-  # noinspection PyMethodMayBeStatic
-  def postload(self, data, componentId, **_) -> Callable:
+  def postload(self, data: Dict, componentId: str, **_) -> Dict:
     """ Delegate to a subclass based on componentId field """
     filters = {f.__name__.lower(): f for f in Filter.__subclasses__()}
     assert componentId in filters, f"Error: Filter type {componentId} doesn't exist in {filters.values()}"
@@ -38,12 +38,12 @@ class FieldFilterMixin:
   field = fields.String(required=True)
 
   @staticmethod
-  def access(item, field) -> Any:
+  def access(item, field: str) -> Any:
     for field_name in field.split("."):
       assert not field_name.startswith("_"), "Private access detected."
 
       # get array-calls
-      m = match("(.*)\[(.*)\\]", field)
+      m = match(r"(.*)\[(.*)\\]", field)
       if m:
         field_name, field_idx = m.groups()
         item = getattr(item, field_name).__getitem__(field_idx)
@@ -54,14 +54,15 @@ class FieldFilterMixin:
     return item
 
 
-
 ALLOWED_OPERATORS = {"AND": and_, "OR": or_}
+
+
 class Group(Filter):
   operator = fields.String(required=True, validate=lambda o: o in ALLOWED_OPERATORS)
   children = fields.Nested(Filter, many=True)
 
-  # noinspection PyMethodMayBeStatic
-  def postload(self, data, children, operator, **_):
+  # noinspection PyMethodOverriding
+  def postload(self, data: Dict, children: List, operator: str, **_):
     if not children:
       return lambda _: True
     children = [Filter().load(child)["filt"] for child in children]
@@ -73,8 +74,8 @@ class Range(Filter, FieldFilterMixin):
   min = fields.Number()
   max = fields.Number()
 
-  # noinspection PyMethodMayBeStatic
-  def postload(self, data, field, min, max, **_):
+  # noinspection PyMethodOverriding, PyShadowingBuiltins
+  def postload(self, data, field: str, min: float, max: float, **_):
     return lambda item: min <= FieldFilterMixin.access(item, field) <= max
 
 
@@ -84,13 +85,18 @@ class Text(Filter, FieldFilterMixin):
   match_case = fields.Boolean(required=False, missing=False)
   match_exact = fields.Boolean(required=False, missing=False)
 
-  # noinspection PyMethodMayBeStatic
-  def postload(self, data, field, values, match_case, match_exact, **_):
+  # noinspection PyMethodOverriding
+  def postload(self, data: Dict, field: str, values: List[str], match_case: bool, match_exact: bool, **_):
     compare = eq if match_exact else str.__contains__
     if match_case:
-      compare = compare(b.lower(), a.lower())
+      compare = lambda a, b: compare(b.lower(), a.lower())  # nopep8    # ignore lambda assign
     return lambda item: any(compare(FieldFilterMixin.access(item, field), value) for value in values)
 
 
 class Checkbox(Filter):
-  """ TODO: not sure for what? """
+  values = fields.Dict(fields.String(), fields.Boolean(), required=True)
+
+  # noinspection PyMethodOverriding
+  def postload(self, data, values: Dict[str, bool], **_):
+    return lambda item: all([FieldFilterMixin.access(item, key) == value
+                             for key, value in values.items()])
