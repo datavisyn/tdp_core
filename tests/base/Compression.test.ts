d7c2f7cea03e8a88fa6cdc8db33334f37cb93aa9
Holger Stitz <holger.stitz@datavisyn.io>
2021-12-06 23:53:00 +0100

    Add compression func to remove consecutive nodes

---
+++ tests/base/Compression.test.ts
@@ -0,0 +1,38 @@
+/// <reference types="jest" />
+import {Compression} from '../../src/base/Compression';
+import {ActionMetaData, ActionNode} from '../../src/provenance';
+
+describe('action compressor', () => {
+
+  const path: ActionNode[] = [
+    new ActionNode(new ActionMetaData('', '', 'action1'), 'selection1', () => null),
+    new ActionNode(new ActionMetaData('', '', 'action2'), 'selection2', () => null),
+    new ActionNode(new ActionMetaData('', '', 'action3'), 'selection2', () => null), // consecutive duplicate
+    new ActionNode(new ActionMetaData('', '', 'action4'), 'selection3', () => null),
+    new ActionNode(new ActionMetaData('', '', 'action5'), 'selection3', () => null), // consecutive duplicate
+    new ActionNode(new ActionMetaData('', '', 'action6'), 'selection2', () => null),
+  ];
+
+
+  it('ground truth', () => {
+    expect(path.length).toBe(6);
+    expect(path.toString()).toBe('action1,action2,action3,action4,action5,action6');
+  });
+
+  it('lastOnly()', () => {
+    // remove only consecutive nodes of `selection2` but not `selection3`
+    const newPath = Compression.lastOnly(path, 'selection2', (p) => p.f_id);
+    expect(newPath.length).toBe(4);
+    expect(newPath[newPath.length - 1].toString()).toBe('action6');
+    expect(newPath.toString()).toBe('action1,action4,action5,action6'); // missing action2 and action3
+  });
+
+  it('removeConsecutiveNodes()', () => {
+    // remove only consecutive nodes of `selection2` but not `selection3`
+    const newPath = Compression.removeConsecutiveNodes(path, 'selection2', (p) => p.f_id);
+    expect(newPath.length).toBe(5);
+    expect(newPath[newPath.length - 1].toString()).toBe('action6');
+    expect(newPath.toString()).toBe('action1,action3,action4,action5,action6'); // missing action2 only
+  });
+
+});
