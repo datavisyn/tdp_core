7ea07f71e7d96629a6a09713d69943a1ac45f863
dvzacharycutler <zachary.cutler@datavisyn.io>
2022-02-03 12:09:27 +0100

    editing exports

--- dist/vis/index.d.ts
+++ dist/vis/index.d.ts
@@ -1,4 +1,5 @@
 export * from './interfaces';
 export * from './Vis';
+export * from './VisSidebar';
 export * from './sidebar';
 //# sourceMappingURL=index.d.ts.map
--- src/vis/index.ts
+++ src/vis/index.ts
@@ -1,3 +1,4 @@
 export * from './interfaces';
 export * from './Vis';
+export * from './VisSidebar';
 export * from './sidebar';
