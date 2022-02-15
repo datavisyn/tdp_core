c31e1a652c434570558b7f96a20a8616185d6361
Michael Puehringer <michael.puehringer@datavisyn.io>
2022-02-15 08:14:02 +0100

    Updated dist files

--- dist/vis/Vis.js
+++ dist/vis/Vis.js
@@ -44,6 +44,7 @@ export function Vis({ columns, selected = {}, colors = [
         if (isBar(visConfig)) {
             setVisConfig(barMergeDefaultConfig(columns, visConfig));
         }
+        // DANGER:: this useEffect should only occur when the visConfig.type changes. adding visconfig into the dep array will cause an infinite loop.
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [visConfig.type]);
     const scales = React.useMemo(() => {
