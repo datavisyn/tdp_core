9d1d242cc13b1b1842bdb0cfa4854e0a1f69874c
Michael Puehringer <michael.puehringer@datavisyn.io>
2022-03-29 11:12:30 +0200

    Added all files to package_data

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
   # installed, specify them here.  If using Python 2.6 or less, then these
   # have to be included in MANIFEST.in as well.
   package_data={
-    pkg['name']: []
+    pkg['name']: ['*/**/*']
   },
 
   # Although 'package_data' is the preferred approach, in some case you may
