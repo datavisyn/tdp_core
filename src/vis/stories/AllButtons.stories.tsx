4c8275fa2b7dd0f4931f14d00d184fc4f2260ed6
Florian Engertsberger <florian.engertsberger@datavisyn.io>
2022-05-02 12:58:50 +0200

    fix background

--- src/vis/stories/AllButtons.stories.tsx
+++ src/vis/stories/AllButtons.stories.tsx
@@ -7,12 +7,6 @@ export default {
   title: 'Example/Styles/Buttons',
   component: AllButtons,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: {
-      options: ['bg-light', 'bg-dark'],
-      control: { type: 'radio' },
-    },
-  },
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
