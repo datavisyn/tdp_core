66e622eda75e7710d19ace6e3d742e63bf3898d2
Michael Puehringer <michael.puehringer@datavisyn.io>
2022-05-10 14:32:06 +0200

    Changed flask exception handler to return json

--- tdp_core/server/utils.py
+++ tdp_core/server/utils.py
@@ -1,9 +1,9 @@
+import http
 import logging
 import time
 
 import werkzeug
-from fastapi import HTTPException
-from flask import Flask
+from flask import Flask, jsonify
 
 from .. import manager
 
@@ -21,19 +21,26 @@ def init_legacy_app(app: Flask):
         return
 
     if hasattr(app, "debug"):
-        app.debug = manager.settings.is_development_mode
+        # TODO: Evaluate if this should be set to manager.settings.is_development_mode
+        app.debug = False
 
     if manager.settings.tdp_core:
         app.config["SECRET_KEY"] = manager.settings.secret_key
 
     @app.errorhandler(werkzeug.exceptions.HTTPException)
     @app.errorhandler(Exception)
-    def handle_exception(e):
-        """Raises a proper fastapi#HTTPException instead of HTML for HTTP errors and exceptions."""
-        _log.exception("An exception in a Flask app", exc_info=e)
-        if isinstance(e, werkzeug.exceptions.HTTPException):
-            raise HTTPException(status_code=e.code, detail=e.description)
-        raise HTTPException(status_code=500, detail=str(e) if manager.settings.is_development_mode else "Internal server error")
+    async def handle_exception(e):
+        """Handles Flask exceptions by returning the same JSON response as FastAPI#HTTPException would."""
+        _log.exception("An error occurred in Flask")
+        # Extract status information if a Flask#HTTPException is given, otherwise return 500 with exception information
+        status_code = e.code if isinstance(e, werkzeug.exceptions.HTTPException) else 500
+        detail = (
+            e.description
+            if isinstance(e, werkzeug.exceptions.HTTPException)
+            else (str(e) if manager.settings.is_development_mode else None)
+        )
+        # Exact same response as the one from FastAPI#HTTPException.
+        return jsonify({"detail": detail or http.HTTPStatus(status_code).phrase}), status_code
 
     return app
 
