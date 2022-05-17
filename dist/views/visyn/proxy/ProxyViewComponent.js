81cfdee1957de1482b77a3fa6d418022f6e5a193
dvzacharycutler <zachary.cutler@datavisyn.io>
2022-05-17 10:59:46 +0200

    adding loading spinner

--- dist/views/visyn/proxy/ProxyViewComponent.js
+++ dist/views/visyn/proxy/ProxyViewComponent.js
@@ -1,6 +1,6 @@
 // Gets into the phovea.ts
 import * as React from 'react';
-import { useMemo } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { I18nextManager } from '../../../i18n/I18nextManager';
 /**
  * This simple proxy view is intended to be used inside of a visyn view component which does the required mapping between
@@ -10,7 +10,22 @@ export function ProxyViewComponent({ site, argument, currentId }) {
     const editedSite = useMemo(() => {
         return site.replace(`\${${argument}}`, currentId);
     }, [argument, site, currentId]);
-    return currentId ? (React.createElement("iframe", { className: "w-100 h-100", src: editedSite })) : (React.createElement("div", { className: "d-flex justify-content-center align-items-center  w-100 h-100" },
+    const loadingFrame = useRef(null);
+    const [isLoading, setIsLoading] = useState(true);
+    React.useEffect(() => {
+        const listener = () => {
+            setIsLoading(false);
+        };
+        const currentNode = loadingFrame.current;
+        if (currentNode) {
+            setIsLoading(true);
+            currentNode.addEventListener('load', listener);
+            currentNode.addEventListener('loadstart', listener);
+        }
+        return () => currentNode === null || currentNode === void 0 ? void 0 : currentNode.removeEventListener('load', listener);
+    }, [loadingFrame, site, argument, currentId]);
+    return currentId ? (React.createElement("div", { className: `w-100 h-100 ${isLoading ? 'tdp-busy' : ''}` },
+        React.createElement("iframe", { ref: loadingFrame, className: "w-100 h-100", src: editedSite }))) : (React.createElement("div", { className: "d-flex justify-content-center align-items-center  w-100 h-100" },
         React.createElement("div", { className: "flex-grow-1 text-center emptyViewText" }, I18nextManager.getInstance().i18n.t('tdp:core.views.emptyProxyView'))));
 }
 //# sourceMappingURL=ProxyViewComponent.js.map
--- src/views/visyn/proxy/ProxyViewComponent.tsx
+++ src/views/visyn/proxy/ProxyViewComponent.tsx
@@ -1,6 +1,6 @@
 // Gets into the phovea.ts
 import * as React from 'react';
-import { useMemo } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { I18nextManager } from '../../../i18n/I18nextManager';
 
 export interface IProxyViewProps {
@@ -22,8 +22,29 @@ export function ProxyViewComponent({ site, argument, currentId }: IProxyViewProp
     return site.replace(`\${${argument}}`, currentId);
   }, [argument, site, currentId]);
 
+  const loadingFrame = useRef<HTMLIFrameElement>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  React.useEffect(() => {
+    const listener = () => {
+      setIsLoading(false);
+    };
+
+    const currentNode = loadingFrame.current;
+
+    if (currentNode) {
+      setIsLoading(true);
+      currentNode.addEventListener('load', listener);
+      currentNode.addEventListener('loadstart', listener);
+    }
+
+    return () => currentNode?.removeEventListener('load', listener);
+  }, [loadingFrame, site, argument, currentId]);
+
   return currentId ? (
-    <iframe className="w-100 h-100" src={editedSite} />
+    <div className={`w-100 h-100 ${isLoading ? 'tdp-busy' : ''}`}>
+      <iframe ref={loadingFrame} className="w-100 h-100" src={editedSite} />
+    </div>
   ) : (
     <div className="d-flex justify-content-center align-items-center  w-100 h-100">
       <div className="flex-grow-1 text-center emptyViewText">{I18nextManager.getInstance().i18n.t('tdp:core.views.emptyProxyView')}</div>
