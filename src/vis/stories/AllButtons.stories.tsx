081e327b6ee6411961e1ebbe30f7c66bcda3b8d5
dvflorianengertsberger <87969942+dvflorianengertsberger@users.noreply.github.com>
2022-06-21 11:47:38 +0200

    Add Storybook stories for custom buttons (#708)
    * create first storybook for button styles
    
    * add story for single button
    
    * change background
    
    * linting
    
    * fix background
    
    * add additional kind of buttons
    
    * typing
    
    * add functions for buttons into stories
    
    * started chaning source of button
    
    * finish Button storybook docu
    
    * linting problems
    
    * linting
    
    * styling of code
    
    * change style to UI
    
    * add comments
    
    * Added build-storybook to build script
    
    * Add tdp_core/dist alias to the storybook webpack config
    
    * added outline buttons, adjusted layout to not contain margin inside a story, added link to styleguide
    
    Co-authored-by: Michael Puehringer <michael.puehringer@datavisyn.io>
    Co-authored-by: Moritz Heckmann <moritz.heckmann@datavisyn.io>

---
+++ src/vis/stories/AllButtons.stories.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { ComponentStory } from '@storybook/react';
+
+// Define function to show all combinations of buttons possible
+function AllButtons() {
+  const themeColors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', 'gray'];
+  return (
+    <div className="container">
+      <h5>
+        <span>
+          Checkout the <i>Button</i> section in the{' '}
+          <a href="https://docs.google.com/document/d/1O3SX49CRacjR2WyteFByPO8rKrFYklh7YJ5FsaBjuDQ/edit#heading=h.h4bfja47lkm7">Styleguide</a> to learn when to
+          use which button style.
+        </span>
+        <hr />
+      </h5>
+
+      <div className="row">
+        <div className="col-3">
+          <h5>.btn-*</h5>
+        </div>
+
+        <div className="col-3">
+          <h5>.btn-text-*</h5>
+        </div>
+
+        <div className="col-3">
+          <h5>.btn-outline-*</h5>
+        </div>
+
+        <div className="col-3">
+          <h5>.btn-icon-*</h5>
+        </div>
+      </div>
+
+      <div>
+        {themeColors.map((color) => {
+          return (
+            <div key={color} className="row pt-2 pb-2">
+              <div className="col-3" style={{ display: 'flex', gap: 6 }}>
+                <button type="button" className={`btn btn-${color}`}>
+                  <i className="fas fa-plus me-1" /> Add
+                </button>
+
+                <button type="button" className={`btn btn-${color}`}>
+                  <i className="fas fa-plus" />
+                </button>
+              </div>
+
+              <div className="col-3" style={{ display: 'flex', gap: 6 }}>
+                <button type="button" className={`btn btn-text-${color}`}>
+                  <i className="fas fa-plus me-1" /> Add
+                </button>
+
+                <button type="button" className={`btn btn-text-${color}`}>
+                  <i className="fas fa-plus" />
+                </button>
+              </div>
+
+              <div className="col-3" style={{ display: 'flex', gap: 6 }}>
+                <button type="button" className={`btn btn-outline-${color}`}>
+                  <i className="fas fa-plus me-1" /> Add
+                </button>
+
+                <button type="button" className={`btn btn-outline-${color}`}>
+                  <i className="fas fa-plus" />
+                </button>
+              </div>
+
+              <div className="col-3">
+                <button type="button" className={`btn btn-icon-${color}`}>
+                  <i className="fas fa-plus" />
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+// Create default export for this story
+export default {
+  title: 'Example/UI/Buttons',
+  component: AllButtons,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+// Create template for story
+// eslint-disable-next-line react/function-component-definition
+const Template: ComponentStory<typeof AllButtons> = () => {
+  return <AllButtons />;
+};
+
+// Create story that is shown
+export const AllButtonsStory = Template.bind({}) as typeof Template;
+AllButtonsStory.parameters = {
+  backgrounds: { default: 'light' },
+};
