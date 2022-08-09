a163fe018b7cc134a52913f68a72e03af6f4ac04
Michael Puehringer <michael.puehringer@datavisyn.io>
2022-08-09 21:20:28 +0200

    Moved phovea_registry to src/phovea_registry.ts

--- src/phovea_registry.ts
+++ src/phovea_registry.ts
@@ -3,10 +3,10 @@
  * Copyright (c) The Caleydo Team. All rights reserved.
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
-import { PluginRegistry } from './src/app';
-import reg from './src/phovea';
+import { PluginRegistry } from './app';
+import reg from './phovea';
 /**
  * build a registry by registering all phovea modules
  */
-//self
+// self
 PluginRegistry.getInstance().register('tdp_core', reg);
