8778f810edabc8f4281b2f45aead437a688224d2
Markus Vogl <markus.vogl@datavisyn.io>
2022-08-23 16:28:08 +0200

    Simplify + fail better + test fixture

--- tdp_core/tests/fixtures/test_db.py
+++ tdp_core/tests/fixtures/test_db.py
@@ -1,16 +1,10 @@
-# create postgresql test db, using newest pg version
-from pathlib import Path
-
 import pytest
 from pytest_postgresql import factories
 from pytest_postgresql.janitor import DatabaseJanitor
 
-# get highest version between 12 and 15, 14 on ubuntu 22.04
-_paths = [f"/usr/lib/postgresql/{version}/bin/pg_ctl" for version in range(12, 16)]
-_path = [p for p in _paths if Path(p).exists()][-1]  # if this fails, no pg_ctl found
 
-# creates a fixture named "postgresql_proc"
-factories.postgresql_proc(executable=_path, dbname="test", password="test")
+# creates a fixture named "postgresql_proc", fails if `pg_config --bindir` is not found
+factories.postgresql_proc()  # postgresql://postgres:@127.0.0.1:<RANDOM_PORT>/tests,
 
 
 @pytest.fixture(scope="session")
@@ -22,3 +16,8 @@ def db_url(postgresql_proc):
 
     yield url
     janitor.drop()
+
+
+@pytest.mark.xfail(raises=FileNotFoundError, reason="Fails if `pg_config --bindir` is not found or fails")
+def test_db_fixture(db_url):
+    assert db_url
