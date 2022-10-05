2352f3be83bfc4d67dd1e389f9580c267a1e2bd0
dvzacharycutler <zachary.cutler@datavisyn.io>
2022-10-06 00:24:09 +0200

    changing data type to a map

--- src/views/visyn/interfaces.ts
+++ src/views/visyn/interfaces.ts
@@ -162,7 +162,7 @@ export type VisynDataViewPluginType<
     /**
      * Data array matching the columns defined in the `dataDesc`.
      */
-    data: Record<string, unknown>[];
+    data: Record<string, Record<string, unknown>>;
     /**
      * Data column description describing the given `data`.
      * TODO:: Type to IReprovisynServerColumn when we merge that into tdp_core
