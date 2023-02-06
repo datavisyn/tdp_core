7b7aec172f6c30ec01446345275626a6923420a0
dvzacharycutler <zachary.cutler@datavisyn.io>
2023-02-06 14:46:08 +0100

    changing name

--- cypress/e2e/health-check.cy.ts
+++ cypress/e2e/health-check.cy.ts
@@ -1,6 +1,6 @@
 describe('Health check for Cypress e2e test', () => {
   it('should visit the home page', () => {
     cy.visit('/');
-    cy.get('body').should('include.text', 'Visualization Type');
+    cy.get('body').should('include.text', 'Visualization type');
   });
 });
