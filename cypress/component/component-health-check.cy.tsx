0e941175af0b88a588089d9c54627833813f371d
Holger Stitz <holger.stitz@datavisyn.io>
2023-02-07 09:53:59 +0100

    fix(cypress): health check component test (#829)
    fix(cypress): health check

--- cypress/component/component-health-check.cy.tsx
+++ cypress/component/component-health-check.cy.tsx
@@ -5,6 +5,6 @@ import { MainApp } from '../../src/demo/MainApp';
 describe('Health check for Cypress component test', () => {
   it('should mount MainApp', () => {
     mount(<MainApp />);
-    cy.get('body').should('include.text', 'Visualization Type');
+    cy.get('body').should('include.text', 'Visualization type');
   });
 });
