594443539e9c134b7b8bb39e7bf26a568bb3775f
Michael Puehringer <michael.puehringer@datavisyn.io>
2023-02-09 12:55:46 +0100

    Added login popup to avoid losing site state

--- src/visynApp/login/VisynLoginForm.tsx
+++ src/visynApp/login/VisynLoginForm.tsx
@@ -1,11 +1,11 @@
-import { ActionIcon, Button, Group, Stack, TextInput, Alert } from '@mantine/core';
+import { ActionIcon, Button, Group, Stack, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleExclamation, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from 'react';
 import { I18nextManager } from '../../i18n/I18nextManager';
 
-export function VisynLoginForm({ onLogin, hasError }: { onLogin: (username: string, password: string) => Promise<void>; hasError: boolean }) {
+export function VisynLoginForm({ onLogin }: { onLogin: (username: string, password: string) => Promise<void> }) {
   const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
   const form = useForm({
     initialValues: {
@@ -15,43 +15,36 @@ export function VisynLoginForm({ onLogin, hasError }: { onLogin: (username: stri
   });
 
   return (
-    <>
-      {hasError ? (
-        <Alert icon={<FontAwesomeIcon icon={faCircleExclamation} />} color="red" radius="md">
-          {I18nextManager.getInstance().i18n.t('phovea:security_flask.alertWrongCredentials')}
-        </Alert>
-      ) : null}
-      <form onSubmit={form.onSubmit((values) => onLogin(values.username, values.password))}>
-        <Stack>
-          <TextInput
-            placeholder={I18nextManager.getInstance().i18n.t('phovea:security_flask.username')}
-            label={I18nextManager.getInstance().i18n.t('phovea:security_flask.username')}
-            name="username"
-            autoComplete="username"
-            {...form.getInputProps('username')}
-            required
-          />
-          <TextInput
-            type={isShowPassword ? 'text' : 'password'}
-            placeholder={I18nextManager.getInstance().i18n.t('phovea:security_flask.password')}
-            label={I18nextManager.getInstance().i18n.t('phovea:security_flask.password')}
-            name="password"
-            autoComplete="current-password"
-            {...form.getInputProps('password')}
-            rightSection={
-              <ActionIcon onClick={() => setIsShowPassword(!isShowPassword)}>
-                {isShowPassword ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
-              </ActionIcon>
-            }
-            required
-          />
-        </Stack>
-        <Group position="right">
-          <Button fullWidth={false} mt="md" type="submit" className="btn btn-primary">
-            {I18nextManager.getInstance().i18n.t('tdp:core.visynApp.loginButton')}
-          </Button>
-        </Group>
-      </form>
-    </>
+    <form onSubmit={form.onSubmit((values) => onLogin(values.username, values.password))}>
+      <Stack>
+        <TextInput
+          placeholder={I18nextManager.getInstance().i18n.t('phovea:security_flask.username')}
+          label={I18nextManager.getInstance().i18n.t('phovea:security_flask.username')}
+          name="username"
+          autoComplete="username"
+          {...form.getInputProps('username')}
+          required
+        />
+        <TextInput
+          type={isShowPassword ? 'text' : 'password'}
+          placeholder={I18nextManager.getInstance().i18n.t('phovea:security_flask.password')}
+          label={I18nextManager.getInstance().i18n.t('phovea:security_flask.password')}
+          name="password"
+          autoComplete="current-password"
+          {...form.getInputProps('password')}
+          rightSection={
+            <ActionIcon onClick={() => setIsShowPassword(!isShowPassword)}>
+              {isShowPassword ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
+            </ActionIcon>
+          }
+          required
+        />
+      </Stack>
+      <Group position="right">
+        <Button fullWidth={false} mt="md" type="submit" className="btn btn-primary">
+          {I18nextManager.getInstance().i18n.t('tdp:core.visynApp.loginButton')}
+        </Button>
+      </Group>
+    </form>
   );
 }
