6249de9ad6a34626257f02ecc020671435b0b9af
Michael Pühringer <51900829+puehringer@users.noreply.github.com>
2024-02-29 13:50:42 +0100

    Revert "feat: rspack" (#872)
    Revert "feat: rspack (#870)"
    
    This reverts commit 5566ddf8c79d60db70a6c6b1af1bfa864a7d132b.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,20 @@
 import { defineConfig } from 'cypress';
-import * as cypressConfig from 'visyn_scripts/config/cypress.config.template';
+import * as webpackConfig from 'visyn_scripts/config/webpack.config';
 
-export default defineConfig(cypressConfig);
+export default defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setupNodeEvents(on, config) {},
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+      webpackConfig: webpackConfig({ workspace_mode: 'single' }, { mode: 'production' }),
+    },
+  },
+});
